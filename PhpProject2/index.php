827a84957887d74fd782393a9cfdecf4f7eb7502
IqbalFahimi <152702349+IqbalFahimi@users.noreply.github.com>
2023-12-02 21:07:52 +0800

    Add files via upload

---
+++ PhpProject2/index.php
@@ -0,0 +1,55 @@
+<?php include 'header.php' ?> 
+<div id="content" class="shadow m-5 p-4 bg-white position-relative">
+    
+    <h4>About Me</h4>
+    <div class="row m-3">
+        <div class="col-4 p-3 text-center">
+            <img src="http://localhost/PhpProject2/pic/myprofile.jpg" style="width:auto;height:200px;" class="img-fluid rounded">
+        </div>
+        
+        <div class="col-7 p-3 rounded" style="background-color: #F5F9BF;">
+            <h4>Introduction</h4><br>
+            <p>
+                My full name is Wan Iqbal Fahimi Bin Wan Mohd Sakri and already turns 22 years old on last 21 July.
+                I was born at Kuala Terengganu, Terengganu on 2001 and currently lives in Kota Bharu, Kelantan.
+                My current role is a full time University Student.
+            </p>
+        </div>
+        
+    </div><br>
+    
+    <div class="row m-3">
+        <div class="col m-2 p-3 rounded" style="background-color: #F5F9BF;">
+            <h4>Education</h4><br>
+            <p>
+                I took a Sijil Pelajaran Malaysia (SPM) at SMK Tengku Lela Segara and continued Sijil Tinggi Pelajaran Malaysia (STPM) in Sports Science at the same place.
+                After finished the STPM, i decided to continue my studies journey at Universiti Sultan Zainal Abidin (UniSZA) in Bachelor Of Information Technology (Informatics Media) With Honors.
+                I am currently in a years 2 of 3 and a half years in this course.
+            </p>
+        </div>
+        
+        <div class="col m-2 p-3 rounded" style="background-color: #F5F9BF;">
+            <h4>Passions & Interests</h4><br>
+            <p>
+                I hardly loves making and editing something like videos. I learned many skills and techniques on how to edit and make a video by myself.
+                This is the main reason why i choose to further my studies in my current course to develop more in the future.
+            </p>
+        </div>
+        
+    </div><br>
+    
+    <div class="row m-3">
+        <div class="col m-2 p-3 rounded" style="background-color: #F5F9BF;">
+            <h4>Fun Fact</h4><br>
+            <p>
+                I really likes sports. I play many kind of sports like a badminton, football, volleyball, sepak takraw and so on.
+                This was my main point when i took sports science during STPM. However i got myself a worse injury on my knee during STPM and need to slowing down in sports.
+                I tried to get a proper fully recover form the injury but still it leaves a long terms impact on my fitness level.
+                That is my turning point in my life, it changes my life journey and i need to keep moving forward from the past.
+            </p>
+        </div>
+        
+    </div><br>
+</div>
+
+<?php include 'footer.php' ?> 
---
+++ PhpProject2/list_project.php
@@ -0,0 +1,43 @@
+<?php include 'header.php' ?> 
+<div class="shadow m-5 p-4 bg-white position-relative">
+    <h4 class="m-3">My Project Lists</h4>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Icon</th>
+                <th>Details</th>
+                <th>Attachment</th>
+                <th>Link</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <tr>
+                <td>JAKA</td>
+                <td><img src="http://localhost/PhpProject2/pic/jaka.png" style="width:auto;height:50px;" class="rounded"></td>
+                <td>The Mascot of UniSZA to welcome new intake's student</td>
+                <td><a href="http://localhost/PhpProject2/pic/JAKA The Mascot.pdf" download>JAKA The Mascot</a> </td>
+                <td> - </td>
+            </tr>
+            
+            <tr>
+                <td>Wet2U</td>
+                <td><img src="http://localhost/PhpProject2/pic/wet.png" style="width:auto;height:50px;" class="rounded"></td>
+                <td>A complete program that organize and estimate the total of amount and price</td>
+                <td><a href="http://localhost/PhpProject2/pic/Wet2u Program.pdf" download>Wet2u Program</a> </td>
+                <td> - </td>
+            </tr>
+            
+            <tr>
+                <td>Song Sorter</td>
+                <td><img src="http://localhost/PhpProject2/pic/song.png" style="width:auto;height:50px;" class="rounded"></td>
+                <td>A complete program that allow you to collect and sort the song and its details</td>
+                <td><a href="http://localhost/PhpProject2/pic/Song Sorter Program.pdf" download>Song Sorter Program</a> </td>
+                <td> - </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<?php include 'footer.php' ?> 
